ff276071dbfe41a3f85e06b4f7b11030b0a9b9a3
boak123 <boluwatifeakorede17@gmail.com>
2024-03-17 08:02:50 -0700

    101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,24 @@
-def safely_get_value(dct: Mapping,
-                     key: Any,
-                     default: Union[F, None]) -> Union[Any, F]:
-    """ Type-annotated function safe_first_element. """
+#!/usr/bin/env python3
+"""
+    Duck typing Typevar
+"""
+from typing import Mapping, TypeVar, Any, Union
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None)\
+        -> Union[Any, T]:
+    """
+        Args:
+            dct: Mapping
+            key: Any data type
+            default: Default value
+
+        Return:
+            Any or T format
+    """
     if key in dct:
         return dct[key]
     else:

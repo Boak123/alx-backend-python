f29ef776feb1fdc5f93b5c50bedafa1f52597d01
boak123 <boluwatifeakorede17@gmail.com>
2024-03-16 17:38:37 -0700

    floor flooat

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Basic annotation - float """
+
+
+def floor(n: float) -> float:
+    Type-annotated function floor that takes a float arguments.
+        Args:
+            n: float type
+        Return:
+            floor of the float
+    """
+    return int(n)

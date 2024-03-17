3b482342b202b85c7c9de76317a329a52cf8be10
boak123 <boluwatifeakorede17@gmail.com>
2024-03-16 18:24:31 -0700

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""" Complex type - list of floats """
-from typing import list
+""" Complex types - list of floats """
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:

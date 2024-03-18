310ba982555c67ed3c9f6d960e94b3a00fe1ccee
boak123 <boluwatifeakorede17@gmail.com>
2024-03-18 15:32:35 -0700

    async

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" Multiple coroutines at the same time with async """
+""" Tasks """
 import asyncio
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Async routine.
         Args:
             max_delay: integer argument.
@@ -18,7 +18,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     orderList: List[float] = []
 
     for i in range(n):
-        delays.append(wait_random(max_delay))
+        delays.append(task_wait_random(max_delay))
 
     for o in asyncio.as_completed(delays):
         orderList.append(await o)

aab4ab4370c44f0798d831dfcb823f2d9a2f97a2
boak123 <boluwatifeakorede17@gmail.com>
2024-04-08 23:11:20 +0000

    test access nested map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """ Parameterize a unit test """
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
@@ -18,15 +18,6 @@ class TestAccessNestedMap(unittest.TestCase):
         nested_map={"a": 1}, path=("a",)
         nested_map={"a": {"b": 2}}, path=("a",)
         nested_map={"a": {"b": 2}}, path=("a", "b")])
-    def test_access_nested_map(self, nested_map, path, answer):
-        """ Call the access_nested_map 
-        function with the input data """
-
-        result = access_nested_map(nested_map, path)
-
-        """ Assert that the result matches the expected output """
-        self.assertEqual(access_nested_map(nested_map, path), answer)
-    
-    if __name__ == '__main__':
-        unittest.main()
-
+    def test_access_nested_map(self, nested_map, path, expected):
+         """Tests `access_nested_map`'s output."""
+         self.assertEqual(access_nested_map(nested_map, path), expected)
